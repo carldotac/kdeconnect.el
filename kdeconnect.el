ec4914e5e782a3e7eb11a925df267664173f0dec
Carl Lieberman <dev@carl.ac>
2018-01-26 18:40:06 -0500

    Increase version number

--- kdeconnect.el
+++ kdeconnect.el
@@ -4,7 +4,7 @@
 
 ;; Author: Carl Lieberman <dev@carl.ac>
 ;; Keywords: kdeconnect, android
-;; Version: 1.2.0
+;; Version: 1.2.1
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
