daee28249b852cc52f4f32d35704a97af5cc4b7d
Carl Lieberman <carldotac@users.noreply.github.com>
2026-03-15 19:59:55 -0400

    Update version to 1.4.0, copyright dates

--- kdeconnect.el
+++ kdeconnect.el
@@ -1,10 +1,10 @@
 ;;; kdeconnect.el --- An interface for KDE Connect  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2016-2023 Carl Lieberman
+;; Copyright (C) 2016-2026 Carl Lieberman
 
 ;; Author: Carl Lieberman <dev@carl.ac>
 ;; Keywords: convenience
-;; Version: 1.3.0
+;; Version: 1.4.0
 ;; URL: https://github.com/carldotac/kdeconnect.el
 ;; Package-Requires: ((emacs "25.1"))
 
